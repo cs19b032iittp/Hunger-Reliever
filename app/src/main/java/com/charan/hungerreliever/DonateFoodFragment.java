b262e9c0c5f8eddec1e552f622658781f4cec4f1
cs19b005-gautam <bgautam1222@gmail.com>
2021-09-05 11:13:17 +0530

    fragment_donate_food.xml and DonateFoodFragment.java updated - BY GAUTAM

--- app/src/main/java/com/charan/hungerreliever/DonateFoodFragment.java
+++ app/src/main/java/com/charan/hungerreliever/DonateFoodFragment.java
@@ -15,7 +15,7 @@ import androidx.fragment.app.Fragment;
 public class DonateFoodFragment extends Fragment {
 
     private Button donateFoodBtn;
-    private EditText food, adders, quantity;
+    private EditText food, adders, quantity, address, city;
 
     @Nullable
     @Override
@@ -25,6 +25,8 @@ public class DonateFoodFragment extends Fragment {
         food = view.findViewById(R.id.food_name);
         quantity = view.findViewById(R.id.food_quantity);
         adders = view.findViewById(R.id.food_adders);
+        address = view.findViewById(R.id.user_current_address);
+        city = view.findViewById(R.id.user_city);
 
 //TODO : get user details to this fragment
 
@@ -33,7 +35,7 @@ public class DonateFoodFragment extends Fragment {
             public void onClick(View view) {
 
 //TODO : add database requirement
-                Toast.makeText(getActivity(), food.getText()+" "+quantity.getText()+" "+adders.getText(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), food.getText()+" "+quantity.getText()+" "+adders.getText()+" "+address.getText()+" "+city.getText(), Toast.LENGTH_SHORT).show();
             }
         });
         return view;
