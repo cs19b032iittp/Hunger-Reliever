125d5fdf82a1727ac74f4156f10c4b85c5529d2b
cs19b005-gautam <74318303+cs19b005-gautam@users.noreply.github.com>
2022-04-24 21:35:13 +0530

    Update CompletedDonations.java

--- app/src/main/java/com/charan/hungerreliever/CompletedDonations.java
+++ app/src/main/java/com/charan/hungerreliever/CompletedDonations.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
